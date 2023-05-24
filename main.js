432d39a7f015fe7c125b4f8e61177bf72322869c
brennoDinaroski <brenno.dinaroski@gmail.com>
2023-05-24 15:59:05 -0300

    css improvements, js first button test, html updates

---
+++ main.js
@@ -0,0 +1,7 @@
+const buttonNext01 = document.querySelector("[data-next01]")
+
+buttonNext01.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('buttonNext01 Clicado')
+})
+
