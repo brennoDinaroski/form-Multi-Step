286bb13157afb13f8f3d0de7ab8a1c7b24de157f
brennoDinaroski <brenno.dinaroski@gmail.com>
2023-05-25 16:14:57 -0300

    JS: Correções de variáveis para formAnterior, formAtual e formProxima

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 // const buttonNext01 = document.querySelector("[data-next01]")
 const buttonsNext = document.querySelectorAll('.buttonNext')
+const buttonsPrevious = document.querySelectorAll('.buttonPrevious')
 const form01 = document.getElementById("form01")
 const form02 = document.getElementById("form02")
 
 let contadorDeStep = 1;
 let formProxima = null;
+let formAtual = null;
 
 buttonsNext.forEach(button => {
     button.addEventListener('click', clicarBotaoNext);
@@ -14,19 +16,20 @@ function clicarBotaoNext(event) {
     event.preventDefault(); 
     console.log('Botao next clicado');
     
-    contadorDeStep = contadorMais(contadorDeStep);  
-    console.log('contador de Step é: ', contadorDeStep); 
-    
-    formProxima = `form0${contadorDeStep}`
-    console.log('formProxima é: ', formProxima);
-    
-    
-    colocaApareceNoForm(formProxima);   
-    
-    formAtual = `form0${contadorDeStep-1}`
-    console.log('a form atual é: ', formAtual);
+    contadorDeStep++;      
 
-    limparClasseApareceNoForm(formAtual); 
+    formAnterior = `form0${contadorDeStep-1}`   
+    formAtual = `form0${contadorDeStep}`
+    formProxima = `form0${contadorDeStep+1}`
+
+    console.log(
+        'form atual: ', formAtual,
+        'form proxima: ', formProxima,
+        'contador de step', contadorDeStep,
+    );
+
+    colocaApareceNoForm(formAtual);   
+    limparClasseApareceNoForm(formAnterior);       
 };
 
 function armazenaContadorDeStep(contadorDeStep) {
@@ -34,21 +37,42 @@ function armazenaContadorDeStep(contadorDeStep) {
     return contadorDeStepArmazenado;
 }
 
-function contadorMais(i) {
-    i++
-    return i;
-}
-
-function colocaApareceNoForm (formProxima) {    
-    const formProximaConst = document.getElementById(formProxima)    
-    formProximaConst.classList.add("aparece");
+function colocaApareceNoForm (formAtual) {    
+    const formAtualConst = document.getElementById(formAtual)    
+    formAtualConst.classList.add("aparece");
 };
 
-function limparClasseApareceNoForm(formAtual) {
-    const formAtualConst = document.getElementById(formAtual)
-    console.log('limparClasseAparece na ', formAtualConst);
-    formAtualConst.classList.remove("aparece");
+function limparClasseApareceNoForm(formAnterior) {
+    const formAnteriorConst = document.getElementById(formAnterior)
+    console.log('limparClasseAparece na ', formAnteriorConst);
+    formAnteriorConst.classList.remove("aparece");
 }
+
+buttonsPrevious.forEach(button => {
+    button.addEventListener('click', clicarBotaoPrevious);
+});
+
+function clicarBotaoPrevious (event) {
+    event.preventDefault();
+    console.log('cliquei Previous');
+    
+    colocaApareceNoForm(formAtual);    
+    
+    contadorDeStep = contadorDeStep -1    
+    formAtual = `form0${contadorDeStep}`  
+    
+    contadorDeStep--;  
+    console.log('contador de Step é: ', contadorDeStep); 
+    
+    formProxima = `form0${contadorDeStep}`
+    console.log('formProxima é: ', formProxima);
+
+
+    console.log('form proxima ANTERIOR: ', formProxima);
+    limparClasseApareceNoForm(formProxima);
+    console.log('form proxima POSTERIOR: ', formProxima);
+}
+
 
 
 
