fad51b2a5205043a21eb9bf5d65808941bbbc07a
brennoDinaroski <brenno.dinaroski@gmail.com>
2023-05-25 15:06:55 -0300

    JS: Botao next funcionando para >adicionar a classe 'aparece' no proximo formulario >apagar a classe 'aparece' no formulario atual Conclusao: clicando em next, vai para o proximo formulario

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
-const buttonNext01 = document.querySelector("[data-next01]")
+// const buttonNext01 = document.querySelector("[data-next01]")
+const buttonsNext = document.querySelectorAll('.buttonNext')
 const form01 = document.getElementById("form01")
 const form02 = document.getElementById("form02")
 
 let contadorDeStep = 1;
 let formProxima = null;
 
-buttonNext01.addEventListener('click', (event) => {
-    event.preventDefault();     
+buttonsNext.forEach(button => {
+    button.addEventListener('click', clicarBotaoNext);
+});
 
-    //quando clicar no next, limpe a palavra 'aparecer' a classList do formulario Atual   
-
-    contadorDeStep = contadorMais(contadorDeStep);   
-
+function clicarBotaoNext(event) {
+    event.preventDefault(); 
+    console.log('Botao next clicado');
+    
+    contadorDeStep = contadorMais(contadorDeStep);  
+    console.log('contador de Step é: ', contadorDeStep); 
+    
     formProxima = `form0${contadorDeStep}`
     console.log('formProxima é: ', formProxima);
-
-    // limparClasseApareceNoForm1(); 
-
+    
+    
     colocaApareceNoForm(formProxima);   
-});
+    
+    formAtual = `form0${contadorDeStep-1}`
+    console.log('a form atual é: ', formAtual);
+
+    limparClasseApareceNoForm(formAtual); 
+};
 
 function armazenaContadorDeStep(contadorDeStep) {
     const contadorDeStepArmazenado = contadorDeStep;
@@ -30,15 +39,16 @@ function contadorMais(i) {
     return i;
 }
 
-function limparClasseApareceNoForm1() {
-    console.log('limparClasseApareceNoForm1');
-    form01.classList.remove("aparece");
-}
-
 function colocaApareceNoForm (formProxima) {    
     const formProximaConst = document.getElementById(formProxima)    
     formProximaConst.classList.add("aparece");
 };
+
+function limparClasseApareceNoForm(formAtual) {
+    const formAtualConst = document.getElementById(formAtual)
+    console.log('limparClasseAparece na ', formAtualConst);
+    formAtualConst.classList.remove("aparece");
+}
 
 
 
