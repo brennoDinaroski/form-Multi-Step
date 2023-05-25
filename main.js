da430efe97c2104a0bf9958d062892266f756d9a
brennoDinaroski <brenno.dinaroski@gmail.com>
2023-05-25 14:38:15 -0300

    js: botao Next do step 01 funcionado para ir para a proxima etapa ao adicionar a classe "aparece" no proximo 'form' css: incluso estilo 'some' e 'aparece' html: 'some' e 'aparece' nos forms

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
 const buttonNext01 = document.querySelector("[data-next01]")
+const form01 = document.getElementById("form01")
+const form02 = document.getElementById("form02")
+
+let contadorDeStep = 1;
+let formProxima = null;
 
 buttonNext01.addEventListener('click', (event) => {
-    event.preventDefault();
-    console.log('buttonNext01 Clicado')
-})
+    event.preventDefault();     
+
+    //quando clicar no next, limpe a palavra 'aparecer' a classList do formulario Atual   
+
+    contadorDeStep = contadorMais(contadorDeStep);   
+
+    formProxima = `form0${contadorDeStep}`
+    console.log('formProxima é: ', formProxima);
+
+    // limparClasseApareceNoForm1(); 
+
+    colocaApareceNoForm(formProxima);   
+});
+
+function armazenaContadorDeStep(contadorDeStep) {
+    const contadorDeStepArmazenado = contadorDeStep;
+    return contadorDeStepArmazenado;
+}
+
+function contadorMais(i) {
+    i++
+    return i;
+}
+
+function limparClasseApareceNoForm1() {
+    console.log('limparClasseApareceNoForm1');
+    form01.classList.remove("aparece");
+}
+
+function colocaApareceNoForm (formProxima) {    
+    const formProximaConst = document.getElementById(formProxima)    
+    formProximaConst.classList.add("aparece");
+};
+
+
 
