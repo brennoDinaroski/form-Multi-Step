a933a69de540a77438336ff5c4eb71100f2bb337
brennoDinaroski <brenno.dinaroski@gmail.com>
2023-05-28 17:48:01 -0300

    JS prices and selectedCards added: >To get the prices of the selected card Plan and store in a variable

---
+++ js/prices.js
@@ -0,0 +1,31 @@
+/* pegar o valor dos planos no step 02,
+    o que tiver a classe 'select'
+    armazenar em uma variavel de 'planSelected' 
+
+depois pegar no step 03
+    quais add-ons tem o checkbox preenchidos
+    se o checkbox for selecionado 
+        colocar um valor de data atrribute no HTMl 
+            selecionar todos os checkbox selecionados
+            jogar em um array
+            somar todos os elementos do array em uma variavel de 'soma add-ons'
+
+pegar as variaveis 'planoEscolhido' e 'somaAddOns'
+    somar em outra variavel 'valorTotal'        
+    e colocar esse valor com no html do total
+
+pegar o plano escolhido e os add-ons 
+    armazenar por um id de cada um
+    usar if para mostrar na tela final todos eles 
+        se selecionado, coloque no html da tela final */
+
+/* ########################################################## */
+
+        /* VARIABLES 
+cards: Array of all cards ok
+card: card selected ok
+planSelected: id of the plan selected ok
+cardSelectedValueInteger: value of the plan selected
+ */
+
+
--- js/selectCards.js
+++ js/selectCards.js
@@ -1,17 +1,46 @@
 const cards = [...document.querySelectorAll(".cardList__card")]
+let planSelected = null;
+let cardSelected = null;
+let cardSelectedChildValue = null;
+let cardSelectedValueHTML = null;
+let cardSelectedValueInteger = null;
 
 console.log(cards);
 
 cards.forEach(card => 
-    card.addEventListener('click', () => clickedCard(card))
+    card.addEventListener('click', () => clickedCardAddSelected(card))
 )
 
-function clickedCard(card) {   
+function clickedCardAddSelected(card) {   
     cleanAllClassesSelected(cards) 
     console.log("I've clicked on the card: ", card);    
     card.classList.add('selected')
+    cardSelected = card
+    planSelected = card.id    
+    getCardSelectedValue()
 }
 
 function cleanAllClassesSelected(cards) {
     cards.forEach(card => card.classList.remove('selected'))
+}
+
+function getCardSelectedValue() {
+    cardSelectedChildValue = cardSelected.querySelector(".card__price")
+    console.log('cardSelectedChildValue: ',cardSelectedChildValue);
+    
+    cardSelectedValueHTML = cardSelectedChildValue.innerHTML;
+    console.log('cardSelectedChildValueHTML: ', cardSelectedValueHTML);
+
+    transformStrToInt(cardSelectedValueHTML)    
+}
+
+function transformStrToInt(cardSelectedValueHTML) {
+    let cardSelectedValueString = cardSelectedValueHTML.split(' ');
+    console.log('cardSelectedValueString: ', cardSelectedValueString);
+    
+    cardSelectedValueString.shift()
+    console.log('cardSelectedValueString after slice: ', cardSelectedValueString);   
+
+    cardSelectedValueInteger = parseInt(cardSelectedValueString)
+    console.log('cardSelectedValueInteger: ', cardSelectedValueInteger,' ',  typeof(cardSelectedValueInteger));    
 }
